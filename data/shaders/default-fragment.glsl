1ec09a868168d3410dd0563a29a82bd9b30c8bb2
David Thompson <dthompson2@worcester.edu>
2014-09-06 15:04:59 -0400

    shader: Add color uniform to default fragment shader.
    * data/shaders/default-fragment.glsl: Add color uniform.

--- data/shaders/default-fragment.glsl
+++ data/shaders/default-fragment.glsl
@@ -1,8 +1,9 @@
 #version 130
 
 in vec2 frag_tex;
+uniform vec4 color;
 uniform sampler2D color_texture;
 
 void main (void) {
-    gl_FragColor = texture2D(color_texture, frag_tex);
+    gl_FragColor = texture2D(color_texture, frag_tex) * color;
 }
